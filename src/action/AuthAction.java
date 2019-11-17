746984be3826b78ede744b1be605d55c8b4a5fec
Jumborin <y.f.architect@gmail.com>
2019-11-17 14:39:01 +0900

    getterとsetterをLOMBOKのアノテーションを使うように修正

--- src/action/AuthAction.java
+++ src/action/AuthAction.java
@@ -1,6 +1,10 @@
 package action;
 
 import com.opensymphony.xwork2.Action;
+
+import lombok.Getter;
+import lombok.Setter;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,43 +17,18 @@ import org.apache.logging.log4j.Logger;
 public class AuthAction implements Action {
 
     private Logger logger = LogManager.getLogger(AuthAction.class);
+    @Getter
+    @Setter
     private String userid = "";
+    @Getter
+    @Setter
     private String password = "";
 
     @Override
     public String execute() throws Exception {
 	// TODO 自動生成されたメソッド・スタブ
-	logger.info("入力されたパラメータは「{}」です。", userid);
-	//return "found";
-	return "error";
+	logger.info("「{}」がログインしました。", userid);
+	return "found";
+	// return "error";
     }
-
-    /**
-     * @return userid
-     */
-    public String getUserid() {
-	return userid;
-    }
-
-    /**
-     * @param userid セットする userid
-     */
-    public void setUserid(String userid) {
-	this.userid = userid;
-    }
-
-    /**
-     * @return password
-     */
-    public String getPassword() {
-	return password;
-    }
-
-    /**
-     * @param password セットする password
-     */
-    public void setPassword(String password) {
-	this.password = password;
-    }
-
 }
