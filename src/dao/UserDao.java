a5d9ec9b6d03f187a4b06537d58bee1c2d2303cf
Jumborin <y.f.architect@gmail.com>
2019-11-22 22:43:38 +0900

    UserDaoについて、コメントを修正し、各メソッドの引数も修正。

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -9,15 +9,15 @@ import entity.User;
  * ユーザDAO
  *
  * @author jumborin
- *
  */
 public class UserDao {
     /**
      * ユーザを取得する
      *
-     * @return
+     * @return ユーザ情報
      */
     public User select(User user) {
+	// TODO
 	user.setUserName("田中太郎");
 	return user;
     }
@@ -25,27 +25,30 @@ public class UserDao {
     /**
      * ユーザを登録する
      *
-     * @return
+     * @return 登録結果(true:成功、false:失敗)
      */
     public boolean insert() {
+	// TODO
 	return true;
     }
 
     /**
      * ユーザを更新する
      *
-     * @return
+     * @return 更新結果(true:成功、false:失敗)
      */
     public boolean update() {
+	// TODO
 	return true;
     }
 
     /**
      * ユーザを削除する
      *
-     * @return
+     * @return 削除結果(true:成功、false:失敗)
      */
     public boolean delete() {
+	// TODO
 	return true;
     }
 }
